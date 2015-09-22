ec59393f4887fe03fdea53a2c29bc737b29ea001
Matthew Murphy <matthew.e.murphy@phila.gov>
2015-09-22 13:21:38 -0400

    transition.  Nothing to see here

--- ballotdisplay.php
+++ ballotdisplay.php
@@ -5,11 +5,15 @@
  * @copyright    Copyright (C) 2015 City of Philadelphia Elections Commission
  * @license      GNU/GPL V2
  */
-
+ini_set('display_errors', 0);
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
-$mainframe->registerEvent('onPrepareContent', 'plgContentBallotdisplay');
+$app = JFactory::getApplication();
+if ($app->isSite()) {
+//Only run from the client-side, never the admin side
+    $mainframe->registerEvent('onPrepareContent', 'plgContentBallotdisplay');
+}
 
 function plgContentBallotdisplay(&$row)
 {
@@ -21,6 +25,7 @@ function plgContentBallotdisplay(&$row)
 
 function plgBallotdisplay(&$text)
 {
+    // Quick, cheap chance to back out.
     if (JString::strpos($text, 'Ballotdisplay') === false) {
         return true;
     }
@@ -46,20 +51,23 @@ function plgBallotdisplay(&$text)
 
 function plgBallotdisplayString(&$text)
 {
+    // Quick, cheap chance to back out.
     if (JString::strpos($text, 'Ballotdisplay') === false) {
         return true;
     }
 
-    $search = "({{Ballotdisplay:.*}})";
+    $search = "(<<Ballotdisplay:.*>>)";
 
     while (preg_match($search, $text, $regs, PREG_OFFSET_CAPTURE)) {
 
-        $temp = explode('=', trim(trim($regs[0][0], '{}'), '{}'));
-        $field = explode(':', $temp[0])[1];
-        $value = $temp[1];
+        $temp = explode('=', trim(trim($regs[0][0], '<>'), '<>'));
+        if (sizeof($temp) === 2) {
+            $field = explode(':', $temp[0])[1];
+            $value = $temp[1];
+        }
 
-        if ($content = getBallots($field, $value)) {
-            $text = str_replace($regs[0][0], $content, $text);
+        if ($field && $value && $content = getBallots($field, $value)) {
+            $text = JString::str_ireplace($regs[0][0], $content, $text);
         }
     }
     return true;
@@ -83,24 +91,179 @@ function getBallots($field, $value)
     }
 
     $db->setQuery($query);
-    $results = $db->loadObjectList();
-    if (!sizeof($results)) {
-        return false;
+    try {
+        $results = $db->loadObjectList();
+    } catch (Exception $e) {
+        return "Oops.  Something has goine wrong while looking for <b>$field</b> : <b>$value</b>.";
     }
-
+    if (!sizeof($results)) {
+        return "There are no Sample Ballots available for <b>$field</b> : <b>$value</b>.";;
+    }
     return getContent($results);
 }
 
 function getContent(&$results)
 {
-
     $return = "";
     foreach ($results as $result) {
         $sid = $result->sid;
         if (JString::strpos($result->sid, '%') !== false && JString::strpos($result->sid, '^') !== false) {
-            $sid = trim(str_replace('^', ' ', explode('%', $result->sid)[1]));
+            $sid = JString::trim(JString::str_ireplace('^', ' ', explode('%', $result->sid)[1]));
         }
         $return .= '<li><a href="/ballot_paper/' . $result->file_id . '.pdf" target="_blank">District ' . $sid . '</a></li>';
     }
-    return "<h4>Download Ballots</h4><ul>" . $return . "</ul>";
+    return "<h4>Download Sample Ballots</h4><ul>" . $return . "</ul>";
+}
+
+
+<?php
+/**
+ * @version     $Id: example.php 14401 2010-01-26 14:10:00Z louis $
+ * @package     Joomla
+ * @subpackage  Content
+ * @copyright   Copyright (C) 2005 - 2010 Open Source Matters. All rights reserved.
+ * @license     GNU/GPL, see LICENSE.php
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
+// Check to ensure this file is included in Joomla!
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport( 'joomla.plugin.plugin' );
+
+/**
+ * Example Content Plugin
+ *
+ * @package     Joomla
+ * @subpackage  Content
+ * @since       1.5
+ */
+class plgContentExample extends JPlugin
+{
+
+    /**
+     * Constructor
+     *
+     * For php4 compatability we must not use the __constructor as a constructor for plugins
+     * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
+     * This causes problems with cross-referencing necessary for the observer design pattern.
+     *
+     * @param object $subject The object to observe
+     * @param object $params  The object that holds the plugin parameters
+     * @since 1.5
+     */
+    function plgContentExample( &$subject, $params )
+    {
+        parent::__construct( $subject, $params );
+    }
+
+    /**
+     * Example prepare content method
+     *
+     * Method is called by the view
+     *
+     * @param   object      The article object.  Note $article->text is also available
+     * @param   object      The article params
+     * @param   int         The 'page' number
+     */
+    function onPrepareContent( &$article, &$params, $limitstart )
+    {
+        global $mainframe;
+        if (is_object($article)) {
+            return plgBallotdisplay($article->text);
+        }
+        return plgBallotdisplay($article);
+    }
+
+    /**
+     * Example after display title method
+     *
+     * Method is called by the view and the results are imploded and displayed in a placeholder
+     *
+     * @param   object      The article object.  Note $article->text is also available
+     * @param   object      The article params
+     * @param   int         The 'page' number
+     * @return  string
+     */
+    function onAfterDisplayTitle( &$article, &$params, $limitstart )
+    {
+        global $mainframe;
+
+        return '';
+    }
+
+    /**
+     * Example before display content method
+     *
+     * Method is called by the view and the results are imploded and displayed in a placeholder
+     *
+     * @param   object      The article object.  Note $article->text is also available
+     * @param   object      The article params
+     * @param   int         The 'page' number
+     * @return  string
+     */
+    function onBeforeDisplayContent( &$article, &$params, $limitstart )
+    {
+        global $mainframe;
+
+        return '';
+    }
+
+    /**
+     * Example after display content method
+     *
+     * Method is called by the view and the results are imploded and displayed in a placeholder
+     *
+     * @param   object      The article object.  Note $article->text is also available
+     * @param   object      The article params
+     * @param   int         The 'page' number
+     * @return  string
+     */
+    function onAfterDisplayContent( &$article, &$params, $limitstart )
+    {
+        global $mainframe;
+
+        return '';
+    }
+
+    /**
+     * Example before save content method
+     *
+     * Method is called right before content is saved into the database.
+     * Article object is passed by reference, so any changes will be saved!
+     * NOTE:  Returning false will abort the save with an error.
+     *  You can set the error by calling $article->setError($message)
+     *
+     * @param   object      A JTableContent object
+     * @param   bool        If the content is just about to be created
+     * @return  bool        If false, abort the save
+     */
+    function onBeforeContentSave( &$article, $isNew )
+    {
+        global $mainframe;
+
+        return true;
+    }
+
+    /**
+     * Example after save content method
+     * Article is passed by reference, but after the save, so no changes will be saved.
+     * Method is called right after the content is saved
+     *
+     *
+     * @param   object      A JTableContent object
+     * @param   bool        If the content is just about to be created
+     * @return  void
+     */
+    function onAfterContentSave( &$article, $isNew )
+    {
+        global $mainframe;
+
+        return true;
+    }
+
 }
