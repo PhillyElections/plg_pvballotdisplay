793a40d0bfe9b9d395556d6f87f91ca49f214b2d
Matthew Murphy <matthew.e.murphy@phila.gov>
2015-09-22 15:23:09 -0400

    implemented as plugin class

--- ballotdisplay.php
+++ ballotdisplay.php
@@ -1,123 +1,5 @@
 <?php
 /**
- * @version      $Id: Ballotdisplay.php Matthew Murphy <matthew.e.murphy@phila.gov>
- * @package      PVPlugins
- * @copyright    Copyright (C) 2015 City of Philadelphia Elections Commission
- * @license      GNU/GPL V2
- */
-ini_set('display_errors', 0);
-// no direct access
-defined('_JEXEC') or die('Restricted access');
-
-$app = JFactory::getApplication();
-if ($app->isSite()) {
-//Only run from the client-side, never the admin side
-    $mainframe->registerEvent('onPrepareContent', 'plgContentBallotdisplay');
-}
-
-function plgContentBallotdisplay(&$row)
-{
-    if (is_object($row)) {
-        return plgBallotdisplay($row->text);
-    }
-    return plgBallotdisplay($row);
-}
-
-function plgBallotdisplay(&$text)
-{
-    // Quick, cheap chance to back out.
-    if (JString::strpos($text, 'Ballotdisplay') === false) {
-        return true;
-    }
-
-    $text = explode('<script', $text);
-    foreach ($text as $i => $str) {
-        if ($i == 0) {
-            plgBallotdisplayString($text[$i]);
-        } else {
-            $str_split = explode('</script>', $str);
-            foreach ($str_split as $j => $str_split_part) {
-                if (($j % 2) == 1) {
-                    plgBallotdisplayString($str_split[$i]);
-                }
-            }
-            $text[$i] = implode('</script>', $str_split);
-        }
-    }
-    $text = implode('<script', $text);
-
-    return true;
-}
-
-function plgBallotdisplayString(&$text)
-{
-    // Quick, cheap chance to back out.
-    if (JString::strpos($text, 'Ballotdisplay') === false) {
-        return true;
-    }
-
-    $search = "(<<Ballotdisplay:.*>>)";
-
-    while (preg_match($search, $text, $regs, PREG_OFFSET_CAPTURE)) {
-
-        $temp = explode('=', trim(trim($regs[0][0], '<>'), '<>'));
-        if (sizeof($temp) === 2) {
-            $field = explode(':', $temp[0])[1];
-            $value = $temp[1];
-        }
-
-        if ($field && $value && $content = getBallots($field, $value)) {
-            $text = JString::str_ireplace($regs[0][0], $content, $text);
-        }
-    }
-    return true;
-}
-
-function getBallots($field, $value)
-{
-    $db = &JFactory::getDBO();
-
-    switch ($field) {
-        case 'name':
-            $query = 'SELECT `b`.* from `#__rt_ballot_upload` `b`, `#__rt_election` `e` WHERE `b`.`eid`=`e`.`id` and name=' . $db->quote($value);
-            break;
-
-        case 'id':
-            $query = 'SELECT * FROM `#__rt_ballot_upload` WHERE `eid`=' . (int) $value;
-            break;
-        default:
-            return false;
-            break;
-    }
-
-    $db->setQuery($query);
-    try {
-        $results = $db->loadObjectList();
-    } catch (Exception $e) {
-        return "Oops.  Something has goine wrong while looking for <b>$field</b> : <b>$value</b>.";
-    }
-    if (!sizeof($results)) {
-        return "There are no Sample Ballots available for <b>$field</b> : <b>$value</b>.";;
-    }
-    return getContent($results);
-}
-
-function getContent(&$results)
-{
-    $return = "";
-    foreach ($results as $result) {
-        $sid = $result->sid;
-        if (JString::strpos($result->sid, '%') !== false && JString::strpos($result->sid, '^') !== false) {
-            $sid = JString::trim(JString::str_ireplace('^', ' ', explode('%', $result->sid)[1]));
-        }
-        $return .= '<li><a href="/ballot_paper/' . $result->file_id . '.pdf" target="_blank">District ' . $sid . '</a></li>';
-    }
-    return "<h4>Download Sample Ballots</h4><ul>" . $return . "</ul>";
-}
-
-
-<?php
-/**
  * @version     $Id: example.php 14401 2010-01-26 14:10:00Z louis $
  * @package     Joomla
  * @subpackage  Content
@@ -134,7 +16,8 @@ function getContent(&$results)
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 jimport( 'joomla.plugin.plugin' );
-
+jimport( 'kint.kint' );
+ini_set('display_errors', 1);
 /**
  * Example Content Plugin
  *
@@ -142,7 +25,7 @@ jimport( 'joomla.plugin.plugin' );
  * @subpackage  Content
  * @since       1.5
  */
-class plgContentExample extends JPlugin
+class plgContentBallotdisplay extends JPlugin
 {
 
     /**
@@ -156,8 +39,13 @@ class plgContentExample extends JPlugin
      * @param object $params  The object that holds the plugin parameters
      * @since 1.5
      */
-    function plgContentExample( &$subject, $params )
+    function plgContentBallotdisplay( &$subject, $params )
     {
+/*        $app = JFactory::getApplication();
+        if ($app->isSite()) {
+        //Only run from the client-side, never the admin side
+        }
+*/
         parent::__construct( $subject, $params );
     }
 
@@ -174,9 +62,9 @@ class plgContentExample extends JPlugin
     {
         global $mainframe;
         if (is_object($article)) {
-            return plgBallotdisplay($article->text);
+            return $this->prepBallotDisplay($article->text);
         }
-        return plgBallotdisplay($article);
+        return $this->prepBallotDisplay($article);
     }
 
     /**
@@ -266,4 +154,95 @@ class plgContentExample extends JPlugin
         return true;
     }
 
+    function prepBallotDisplay(&$text)
+    {
+        // Quick, cheap chance to back out.
+        if (JString::strpos($text, 'Ballotdisplay') === false) {
+            return true;
+        }
+
+        $text = explode('<script', $text);
+        foreach ($text as $i => $str) {
+            if ($i == 0) {
+                $this->getBallotdisplayStrings($text[$i]);
+            } else {
+                $str_split = explode('</script>', $str);
+                foreach ($str_split as $j => $str_split_part) {
+                    if (($j % 2) == 1) {
+                        $this->getBallotdisplayStrings($str_split[$i]);
+                    }
+                }
+                $text[$i] = implode('</script>', $str_split);
+            }
+        }
+        $text = implode('<script', $text);
+
+        return true;
+    }
+
+    function getBallotdisplayStrings(&$text)
+    {
+        // Quick, cheap chance to back out.
+        if (JString::strpos($text, 'Ballotdisplay') === false) {
+            return true;
+        }
+
+        $search = "(\[\[Ballotdisplay:.*\]\])";
+
+        while (preg_match($search, $text, $regs, PREG_OFFSET_CAPTURE)) {
+
+            $temp = explode('=', trim(trim($regs[0][0], '[]'), '[]'));
+            if (sizeof($temp) === 2) {
+                $field = explode(':', $temp[0])[1];
+                $value = $temp[1];
+            }
+
+            if ($field && $value && $content = $this->getBallots($field, $value)) {
+                $text = JString::str_ireplace($regs[0][0], $content, $text);
+            }
+        }
+        return true;
+    }
+
+    function getBallots($field, $value)
+    {
+        $db = &JFactory::getDBO();
+
+        switch ($field) {
+            case 'name':
+                $query = 'SELECT `b`.* from `#__rt_ballot_upload` `b`, `#__rt_election` `e` WHERE `b`.`eid`=`e`.`id` and name=' . $db->quote($value);
+                break;
+
+            case 'id':
+                $query = 'SELECT * FROM `#__rt_ballot_upload` WHERE `eid`=' . (int) $value;
+                break;
+            default:
+                return false;
+                break;
+        }
+
+        $db->setQuery($query);
+        try {
+            $results = $db->loadObjectList();
+        } catch (Exception $e) {
+            return "Oops.  Something has goine wrong while looking for <b>$field</b> : <b>$value</b>.";
+        }
+        if (!sizeof($results)) {
+            return "There are no Sample Ballots available for <b>$field</b> : <b>$value</b>.";;
+        }
+        return $this->getContent($results);
+    }
+
+    function getContent(&$results)
+    {
+        $return = "";
+        foreach ($results as $result) {
+            $sid = $result->sid;
+            if (JString::strpos($result->sid, '%') !== false && JString::strpos($result->sid, '^') !== false) {
+                $sid = JString::trim(JString::str_ireplace('^', ' ', explode('%', $result->sid)[1]));
+            }
+            $return .= '<li><a href="/ballot_paper/' . $result->file_id . '.pdf" target="_blank">District ' . $sid . '</a></li>';
+        }
+        return "<h4>Download Sample Ballots</h4><ul>" . $return . "</ul>";
+    }
 }
