aa0b0c6994b33e4e1b88e8aeac7dec9b5cc8c444
Matthew Murphy <matthew.murphy2@phila.gov>
2016-02-12 14:13:28 -0500

    lets use __construct()

--- ballotdisplay.php
+++ ballotdisplay.php
@@ -30,15 +30,11 @@ class plgContentBallotdisplay extends JPlugin
     /**
      * Constructor
      *
-     * For php4 compatability we must not use the __constructor as a constructor for plugins
-     * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
-     * This causes problems with cross-referencing necessary for the observer design pattern.
-     *
      * @param object $subject The object to observe
      * @param object $params  The object that holds the plugin parameters
      * @since 1.5
      */
-    public function plgContentBallotdisplay(&$subject, $params)
+    public function __construct(&$subject, $params)
     {
         parent::__construct($subject, $params);
     }
